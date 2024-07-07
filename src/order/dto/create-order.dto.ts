d90c13c378e09009ec44497add6fdca05479a9be
Tanvir Ahmed Robbany <tanvirahmed80151@gmail.com>
2024-07-08 00:58:02 +0600

    Swagger API Documentation Implemented

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateOrderDto {
+    @ApiProperty({ example: "1", description: "Pizza id" })
     pizza_id: string;
+
+    @ApiProperty({ example: "2", description: "Quantity" })
     quantity: number;
+
+    @ApiProperty({ example: "1", description: "User id" })
     ordered_by: string;
+
+    @ApiProperty({ example: "Address", description: "Full Address"})
     address: string;
 }
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -3,31 +3,59 @@ import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { EventPattern } from '@nestjs/microservices';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('orders')
 @Controller('order')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(private readonly orderService: OrderService) { }
 
+  @ApiOperation({ summary: 'Create an order' })
+  @ApiResponse({ status: 201, description: 'The order has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
+  @ApiOperation({ summary: 'Get all orders' })
+  @ApiResponse({ status: 200, description: 'The orders have been successfully retrieved.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Get()
   findAll() {
     return this.orderService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get an order by ID' })
+  @ApiResponse({ status: 200, description: 'The order has been successfully retrieved.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update an order by ID' })
+  @ApiResponse({ status: 200, description: 'The order has been successfully updated.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiBody({
+    description: 'The new values for the order',
+    schema: {
+      example: {
+        pizza_id: "665d962b7b4a2dff52699a9a",
+        quantity: 3,
+        ordered_by: "Tanvir",
+        address: "Prolific Infotech",
+      }
+    }
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.orderService.update(id, updateOrderDto);
   }
 
+  @ApiOperation({ summary: 'Delete an order by ID' })
+  @ApiResponse({ status: 200, description: 'The order has been successfully deleted.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.orderService.remove(id);
